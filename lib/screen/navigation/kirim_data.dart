db972132b8b744e832c95eb99a778625066de8c7
Azlan-saja <azlansaja19@gmail.com>
2025-03-24 09:41:11 +0700

    p9

--- lib/screen/navigation/kirim_data.dart
+++ lib/screen/navigation/kirim_data.dart
@@ -1,1 +1,24 @@
+import 'package:flutter/material.dart';
 
+class KirimData extends StatelessWidget {
+  final String nama;
+
+  const KirimData({super.key, required this.nama });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Kirim Data')),
+      body:    Center(
+        child: Text(
+          'Nama saya adalah $nama',
+          style: const TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.blueAccent,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screen/navigation/kirim_terima_data.dart
+++ lib/screen/navigation/kirim_terima_data.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
 
+class KirimTerimaData extends StatelessWidget {
+  const KirimTerimaData({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Kirim & Terima Data')),
+      body: const Center(
+        child: Text('Hello AzlansajaTV'),
+      ),
+    );
+  }
+}
